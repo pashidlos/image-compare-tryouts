5f3ca9bf89b3eae4bf23b74a05e19d43ffb298fc
Pavel Strunkin <pashidlos@gmail.com>
2021-06-18 10:18:38 +0300

    odiff udpated

--- src/looksSame.js
+++ src/looksSame.js
@@ -1,5 +1,13 @@
 const looksSame = require("looks-same");
-const { getImagePath, getImageBuffer, saveImage } = require("./utils");
+const { PNG } = require("pngjs");
+const {
+  getImage,
+  getImagePath,
+  getImageBuffer,
+  saveImage,
+  scaleImageToSize,
+  applyIgnoreAreas,
+} = require("./utils");
 
 (async () => {
   // const result = looksSame.createDiff(
@@ -13,26 +21,68 @@ const { getImagePath, getImageBuffer, saveImage } = require("./utils");
   //   }
   // );
 
-  const result = looksSame.createDiff(
-    {
-      reference: getImageBuffer("baseline.png"),
-      current: getImageBuffer("screenshot.png"),
-      highlightColor: "#ff00ff",
-    },
-    (err, buffer) => {
-      console.log("calback");
-      saveImage("diffBuffer.png", buffer);
-    }
-  );
-
-  // const result = await looksSame(
-  //   getImagePath("baseline.png"),
-  //   getImagePath("screenshot.png"),
-  //   (error, result) => {
+  // const result = looksSame.createDiff(
+  //   {
+  //     reference: getImageBuffer("1.png"),
+  //     current: getImageBuffer("2.png"),
+  //     highlightColor: "#ff00ff",
+  //   },
+  //   (err, buffer) => {
   //     console.log("calback");
-  //     console.log(result);
+  //     saveImage("diffBuffer.png", buffer);
   //   }
   // );
 
+  const baseline = getImage("baseline.png");
+  const image = getImage("screenshot.png");
+
+  const maxWidth = Math.max(baseline.width, image.width);
+  const maxHeight = Math.max(baseline.height, image.height);
+  const scaledBaseline = scaleImageToSize(baseline, maxWidth, maxHeight);
+  const scaledImage = scaleImageToSize(image, maxWidth, maxHeight);
+
+  // const baselineIgnored = applyIgnoreAreas(scaledBaseline, data.ignoreAreas);
+  // const imageIgnored = applyIgnoreAreas(scaledImage, data.ignoreAreas);
+
+  const result = await compare(baseline, image);
+  const result = await createDiff(baseline, image);
+
   console.log("done");
 })();
+
+async function compare(baseline, image) {
+  return new Promise((resolve, reject) => {
+    looksSame(
+      PNG.sync.write(baseline),
+      PNG.sync.write(image),
+      null,
+      (error, diffResult) => {
+        if (error) {
+          // reject(error.message);
+          console.log(error.message);
+        }
+        resolve(diffResult);
+        console.log(diffResult);
+      }
+    );
+  });
+}
+
+async function createDiff(baseline, image) {
+  return new Promise((resolve, reject) => {
+    looksSame.createDiff(
+      {
+        reference: PNG.sync.write(baseline),
+        current: PNG.sync.write(image),
+        highlightColor: "#ff00ff",
+      },
+      (error, buffer) => {
+        if (error) {
+          reject(error.message);
+        }
+        saveImage("diff2.png", buffer);
+        resolve();
+      }
+    );
+  });
+}
--- src/utils.js
+++ src/utils.js
@@ -10,8 +10,43 @@ function getImageBuffer(name) {
   return readFileSync(getImagePath(name));
 }
 
+function getImage(name) {
+  return PNG.sync.read(readFileSync(getImagePath(name)));
+}
+
 function saveImage(name, imageBuffer) {
   return writeFileSync(getImagePath(name), imageBuffer);
 }
 
-module.exports = { getImagePath, getImageBuffer, saveImage };
+function scaleImageToSize(image, width, height) {
+  if (width > image.width || height > image.height) {
+    const preparedImage = new PNG({ width, height, fill: true });
+    PNG.bitblt(image, preparedImage, 0, 0, image.width, image.height);
+    return preparedImage;
+  }
+  return image;
+}
+
+function applyIgnoreAreas(image, ignoreAreas) {
+  ignoreAreas.forEach((area) => {
+    for (let y = area.y; y < area.y + area.height; y++) {
+      for (let x = area.x; x < area.x + area.width; x++) {
+        const k = 4 * (image.width * y + x);
+        image.data[k + 0] = 0;
+        image.data[k + 1] = 0;
+        image.data[k + 2] = 0;
+        image.data[k + 3] = 0;
+      }
+    }
+  });
+  return image;
+}
+
+module.exports = {
+  getImagePath,
+  getImageBuffer,
+  saveImage,
+  scaleImageToSize,
+  applyIgnoreAreas,
+  getImage,
+};
